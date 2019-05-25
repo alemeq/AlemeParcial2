9a7abde45999597c2adaf11eb1193e13b3f5c1e0
Alejandra <alemeque.am@gmail.com>
2019-05-25 09:35:47 -0400

    errores en apiService

---
+++ AlemeParcial2/App/App/ViewModel/LoginViewModel.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App.ViewModel
+{
+    class LoginViewModel : BaseViewModel
+    {
+
+        #region variables
+        string content;
+        bool isrunning;
+        bool isenabled;
+        #endregion
+
+        #region propiedades
+        public string Content
+        {
+            get { return this.content; }
+            set { SetValue(ref this.content, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
+        public bool IsEnabled
+        {
+            get { return this.isenabled; }
+            set { SetValue(ref this.isenabled, value); }
+        }
+        #endregion
+
+        #region comandos
+        public ICommand LoginCommand
+        {
+            get { return new RelayCommand(cmdRegister); }
+        }
+        private async void cmdRegister()
+        {
+            if (String.IsNullOrEmpty(Content))
+            {
+                await App.Current.MainPage.DisplayAlert("Register empty", "Please input register", "Accept");
+                return;
+            }
+            IsRunning = true;
+        }
+        #endregion
+
+    }
+}
---
+++ AlemeParcial2/App/App/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ViewModel
+{
+    class MainViewModel
+    {
+        #region Properties
+
+        public string Token { get; set; }
+
+        public string TokenType { get; set; }
+
+        #endregion
+
+        #region ViewModels
+        public LoginViewModel Register { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Register = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
+    }
+}
+
